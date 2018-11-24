554904102fb7a9424f03e2a2a06fecdb7b7ecffd
KOU256 <g15219@g.ichinoseki.ac.jp>
2018-11-24 20:23:21 +0900

    Update main.cpp
    ひとまず解いてみた。

--- DACVA/main.cpp
+++ DACVA/main.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    int n, mini;
+    int n, mini, ans;
     int sum = 0;
     double ave;
     vector<double> a;
@@ -17,9 +18,15 @@ int main(int argc, char **argv) {
     }
     ave = (double)sum / (double)n;
     mini = ave;
-    for (int j = 0; j < n; j++)
-    {
+
+    for (int j = 0; j < n; j++) {
+        if ( abs(ave - a[j]) < mini) {
+            ans = j;
+            mini = abs(ave - a[j]);
+        }
     }
 
-        return 0;
+    cout << ans << endl;
+
+    return 0;
 }
