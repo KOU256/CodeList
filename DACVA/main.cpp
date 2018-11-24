d2c7e708c725d27727bcb7f6877f21f555c715e2
KOU256 <g15219@g.ichinoseki.ac.jp>
2018-11-24 20:43:54 +0900

    Update main.cpp
    ちょこっといじってみた。

--- DACVA/main.cpp
+++ DACVA/main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 int main(int argc, char **argv) {
     int n, ans;
-    int sum = 0;
-    double ave, mini;
+    double min;
+    double ave = 0;
     vector<double> a;
 
     cin >> n;
     a.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-        sum += a[i];
+        ave += a[i];
     }
-    ave = (double)sum / (double)n;
-    mini = ave;
+    ave /= (double)n;
+    min = ave;
 
     for (int j = 0; j < n; j++) {
         a[j] = fabs(a[j] - ave);
-        if (a[j] < mini) {
+        if (a[j] < min) {
             ans = j;
-            mini = a[j];
+            min = a[j];
         }
     }
 
