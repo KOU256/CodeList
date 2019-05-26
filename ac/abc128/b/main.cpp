b514f7f3624b32a02672451a11aac7e13496f84b
KOU256 <g15219@g.ichinoseki.ac.jp>
2019-05-26 22:59:19 +0900

    :sparkles: BをACした
    なぜ気づかなかったのか

--- ac/abc128/b/main.cpp
+++ ac/abc128/b/main.cpp
@@ -13,25 +13,10 @@ int main() {
     array.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> array[i].first >> array[i].second;
+        array[i].second *= -1;
     }
     copy = array;
     sort(array.begin(), array.end());
-
-    for (int j = 0;j < array.size() - 1;j++){
-        if (array[j].first == array[j + 1].first) {
-            int num = 1;
-            while (true) {
-                if (j + num >= array.size()) {
-                    break;
-                }
-                else if (array[j].first != array[j + num].first) {
-                    break;
-                }
-                num++;
-            }
-            partial_sort(array.begin() + j, array.begin() + j + num, array.begin() + j + num, greater<pair<string, int>>());
-        }
-    }
 
     for (int k = 0; k < array.size(); k++) {
         int m = 0;
