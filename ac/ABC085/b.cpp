93f6c88510b377631bdeaa317577fd840f62b444
KOU256 <g15219@g.ichinoseki.ac.jp>
2019-09-01 19:13:26 +0900

    :arrow_right:  ファイルを移動
    フォルダの構成をいじった。

---
+++ ac/ABC085/b.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    int n;
+    vector<int> d;
+
+    cin >> n;
+    d.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> d[i];
+    }
+
+    sort(d.begin(), d.end());
+
+    for (int j = 0; j < d.size() - 1;) {
+        if (d[j] == d[j + 1]) {
+            d.erase(d.begin() + j);
+        }
+        else {
+            j++;
+        }
+    }
+
+    cout << d.size() << endl;
+
+    return 0;
+}
