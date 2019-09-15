09e52556bdb69423e80334432816d4e41c057016
KOU256 <g15219@g.ichinoseki.ac.jp>
2019-09-15 23:09:15 +0900

    :sparkles: ABC038のAを解いた
    ACした｡

---
+++ ac/ABC038/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+
+    cin >> s;
+
+    if (s.back() == 'T') {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
