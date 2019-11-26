40970829a5eaa4bb941b34e4626b76b06365532f
KOU256 <g15219@g.ichinoseki.ac.jp>
2019-11-27 07:09:34 +0900

    :sparkles: ABC081-Bを解いた
    ACした｡

--- ac/ABC081/b.cpp
+++ ac/ABC081/b.cpp
@@ -1,33 +1,32 @@
 #include<iostream>
 #include<vector>
 
-int main(int argc, char **argv) {
-    int n, input;
-    int count = 0;
-    std::vector<int> a;
+using namespace std;
 
-    std::cin >> n;
-    while(n > 0) {
-        std::cin >> input;
-        a.push_back(input);
-        n--;
+int main(int argc, char **argv) {
+    int n;
+    int res = 500000000;
+    vector<int> a;
+
+    cin >> n;
+    a.resize(n);
+    for (auto&& i : a) {
+        cin >> i;
     }
 
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] % 2 == 0) {
-            a[i] /= 2;
-        }
-        else {
-            break;
-        }
-
-        if (i == a.size() - 1) {
-            i = 0;
+    for (auto&& i : a) {
+        int count = 0;
+        while(i % 2 ==0) {
+            i /= 2;
             count++;
         }
+
+        if (res > count) {
+            res = count;
+        }
     }
 
-    std::cout << count << std::endl;
+    cout << res << endl;
 
     return 0;
 }
