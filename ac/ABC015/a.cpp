b7f3f57114cd6a4478e6249633ed7eaa3a4ceda4
KOU256 <g15219@g.ichinoseki.ac.jp>
2020-01-26 00:09:39 +0900

    :sparkles: ABC015の問題を解いた
    A,B,CをACした｡

---
+++ ac/ABC015/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    if (a.length() >= b.length()) {
+        cout << a << endl;
+    }
+    else {
+        cout << b << endl;
+    }
+
+    return 0;
+}
