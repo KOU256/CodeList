c0e433218a229637dbf6b0494ac0fd7da1c2f000
KOU256 <kou256.8bit+github@gmail.com>
2020-09-07 16:16:07 +0900

    :sparkles: ITP1_2を解いた。
    4問全てAcceptedした。

---
+++ aoj/ITP1/2/a.cpp
@@ -0,0 +1,19 @@
+// 2/a.cpp
+#include <bits/stdc++.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a < b) {
+        printf("a < b\n");
+    }
+    else if (a > b) {
+        printf("a > b\n");
+    }
+    else {
+        printf("a == b\n");
+    }
+
+    return 0;
+}
---
+++ aoj/ITP1/2/b.cpp
@@ -0,0 +1,16 @@
+// 2/b.cpp
+#include <bits/stdc++.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a < b && b < c) {
+        printf("Yes\n");
+    }
+    else {
+        printf("No\n");
+    }
+
+    return 0;
+}
